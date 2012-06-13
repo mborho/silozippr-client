2cc50f69e17a6c17a03906166068c8b8180cff57
mborho <martin@borho.net>
2012-06-13 20:20:09 +0200

    toolbar added and login popup

--- source/App.js
+++ source/App.js
@@ -1,10 +1,30 @@
+enyo.Scroller.touchScrolling = !enyo.Scroller.hasTouchScrolling();
+
 enyo.kind({
-	name: "App",
-	fit: true,
-	components:[
-		{name: "hello", content: "Hello World", allowHtml: true, ontap: "helloWorldTap"}
-	],
-	helloWorldTap: function(inSender, inEvent) {
-		this.$.hello.addContent("<br/><b>hello</b> control was tapped");
-	}
+    name: "App",
+    fit: true,
+    components:[
+        {kind: "onyx.Toolbar", components: [
+            { kind: "onyx.Button", content: "Load", ontap: "showLogin" },
+        ]},
+//         {name: "results", kind: "Scroller", horizontal: "hidden", style: "top:55px", classes: "enyo-fit list enyo-unselectable"},
+        {name: "loginPopup", kind: "onyx.Popup", centered: true, modal: true, floating: true, components: [
+//             {content: "Here's some information..."}
+            {kind: "onyx.Groupbox", components: [
+                {kind: "onyx.GroupboxHeader", content: "Login"},
+                    {kind: "onyx.InputDecorator", components: [
+                        {kind: "onyx.Input"}
+                    ]},
+                    {kind: "onyx.InputDecorator", components: [
+                        {kind: "onyx.Input", type:"password"}
+                    ]},
+                    {kind: "onyx.Button", content: "Login", classes: "onyx-affirmative"}
+                ]}
+        ]},
+    ],
+    showLogin: function(inSender, inEvent) {
+        console.log(inSender);
+        console.log(inEvent);                
+        this.$.loginPopup.show();
+    },
 });
