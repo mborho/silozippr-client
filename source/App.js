cfcb52e52c18bcf058e1d08afedc14e2846bafc6
mborho <martin@borho.net>
2012-06-20 13:52:08 +0200

    panel based layout

--- source/App.js
+++ source/App.js
@@ -1,8 +1,12 @@
 enyo.Scroller.touchScrolling = !enyo.Scroller.hasTouchScrolling();
 
+
 enyo.kind({
     name: "App",
-    fit: true,    
+    kind: "Panels",
+    classes: "panels enyo-unselectable",
+    realtimeFit: true,
+    arrangerKind: "CollapsingArranger",
     published: {
         apiEndpoint: "http://couch.borho.net:8990",
         loggedIn: false,
@@ -10,31 +14,54 @@ enyo.kind({
     },
     events: {
         
-    },
-    components:[
-        {kind: "onyx.Toolbar", components: [
-            { name: "buttonLogin", kind: "onyx.Button", content: "Login", ontap: "showLoginPopup"},
-            { name: "buttonLogout", kind: "onyx.Button", content: "Logout", ontap: "sendLogout", showing: false },
+    },    
+    components: [
+
+        {name: "tocPanel", layoutKind: "FittableRowsLayout", components: [                        
+            {kind: "onyx.Toolbar", components: [
+                    { name: "buttonLogin", kind: "onyx.Button", content: "Login", ontap: "showLoginPopup"},
+                    { name: "buttonLogout", kind: "onyx.Button", content: "Logout", ontap: "sendLogout", showing: false },
+            ]},
+            {name: "loginPopup", kind: "onyx.Popup", centered: true, modal: true, floating: true, components: [
+                {kind: "onyx.Groupbox", components: [
+                    {kind: "onyx.GroupboxHeader", content: "Login"},
+                    {kind: "onyx.InputDecorator", components: [
+                        {name: "popupUsername", kind: "onyx.Input"}
+                    ]},
+                    {kind: "onyx.InputDecorator", components: [
+                        {name: "popupPassword", kind: "onyx.Input", type:"password"}
+                    ]},
+                    {kind: "onyx.Button", content: "Login", classes: "onyx-affirmative", onclick: "sendLogin"}
+                ]}
+            ]},
         ]},
-        {name:"mainline", kind:"Mainline"},
-        {name: "loginPopup", kind: "onyx.Popup", centered: true, modal: true, floating: true, components: [
-//             {content: "Here's some information..."}
-            {kind: "onyx.Groupbox", components: [
-                {kind: "onyx.GroupboxHeader", content: "Login"},
-                {kind: "onyx.InputDecorator", components: [
-                    {name: "popupUsername", kind: "onyx.Input"}
+        {name: "contentView", fit: true, kind: "FittableRows", classes: "enyo-fit main", components: [
+                {name: "backToolbar", kind: "onyx.Toolbar", showing: true, components: [
+                        { name: "tocButton", kind: "onyx.Button", content: "Toc", showing: false, ontap: "showToc"},
+                        { name: "homeButton", kind: "onyx.Button", content: "Home", ontap: "refreshMainList"},
                 ]},
-                {kind: "onyx.InputDecorator", components: [
-                    {name: "popupPassword", kind: "onyx.Input", type:"password"}
-                ]},
-                {kind: "onyx.Button", content: "Login", classes: "onyx-affirmative", onclick: "sendLogin"}
-            ]}
-        ]},
+                
+                {name:"mainline", kind:"Mainline"},
+        ]},            
     ],
     create: function() {
         this.inherited(arguments);        
         this.setConnector(new Connector(this.getApiEndpoint()));
         this.checkSession();
+    },   
+    rendered: function() {
+            this.inherited(arguments);
+    },    
+    reflow: function() {
+            this.inherited(arguments);
+            var backShowing = this.$.tocButton.showing;
+            this.$.tocButton.setShowing(enyo.Panels.isScreenNarrow());
+            if (this.$.tocButton.showing != backShowing) {
+                    this.$.contentView.resized();
+            }
+    },    
+    showToc: function() {
+            this.setIndex(0);
     },    
     checkSession: function() {
         new enyo.Ajax({url: this.getApiEndpoint()+"/api/me"}).go().response(this, "sessionStart").error(this, "showLoginPopup");
@@ -61,5 +88,5 @@ enyo.kind({
     },
     sendLogout: function(inSender, inEvent) {
         new enyo.Ajax({url: this.apiEndpoint+"/logout"}).go().response(this, "loggedOut");
-    }
-});
+    },
+});  
