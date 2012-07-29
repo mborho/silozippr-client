f211c2f8fa139cd9f114e5182b79cca843f86ddb
mborho <martin@borho.net>
2012-07-29 18:49:03 +0200

    methods added for ajax calls

--- source/libs/Connector.js
+++ source/libs/Connector.js
@@ -4,6 +4,12 @@ var Connector = (function(apiEndpoint) {
     return {
         getApiEndpoint: function() {
             return _apiEndpoint;
+        },
+        loadToc: function() {
+            return new enyo.Ajax({url: _apiEndpoint+'/api/toc/get'}).go();
+        },
+        loadList: function(params) {
+            return new enyo.Ajax({url: _apiEndpoint+"/api/list/docs"}).go(params);
         }
     }
 });
